9eb8353a92cf52e09e426601ddfb79ccaf0c06e6
marwan alaa <sheetoss94@gmail.com>
2016-09-01 12:28:22 +0200

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)    # Not the final implementation!
    if @user.save
-     flash[:success] = "Welcome to the Sample App!"
+     session[:user_id]=@user.id
+     flash.now[:success] = "Welcome to the Sample App!"
      redirect_to @user
      # Handle a successful save.
    else
